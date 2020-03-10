0838fbb353b4d8faedd6f26ab572c0ba74c0a2a4
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-03-10 17:19:57 -0400

    can move snake up

--- client.js
+++ client.js
@@ -12,7 +12,16 @@ const connect = function() {
   conn.setEncoding('utf8');
   conn.on('connect', () => {
     console.log("Connected to server!");
+
+    // Give three-letter name to be displayed on server
     conn.write('Name: SNK');
+
+    // Sending "Move: up" moves the snake up
+    let up = () => {
+      conn.write('Move: up');
+    };
+    // Can keep moving snake up by using setInterval
+    // setInterval(up, 500)
   });
   conn.on('data', (data) => {
     console.log(data);
