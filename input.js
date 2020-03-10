396eb6b4dda6f62258e1484346aee5e5006bd3f6
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-03-10 17:46:27 -0400

    implemented wasd moving

--- input.js
+++ input.js
@@ -1,9 +1,13 @@
+// Stores the active TCP connection object.
+let connection;
+
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection=conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -16,6 +20,14 @@ const handleUserInput = (key) => {
   // Ctrl+C
   if (key === '\u0003') {
     process.exit();
+  } else if (key === 'w') {
+    connection.write('Move: up');
+  } else if (key === 'a') {
+    connection.write('Move: left');
+  } else if (key === 's') {
+    connection.write('Move: down');
+  } else if (key === 'd') {
+    connection.write('Move: right');
   }
 };
-module.exports = {setupInput};
+module.exports = { setupInput };
