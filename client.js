15bc14d22fae66486bdbee45b4e1ebce8b615caa
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-03-10 17:06:01 -0400

    put connect function in its own module

---
+++ client.js
@@ -0,0 +1,19 @@
+/**
+ * Establishes connection with the game server
+ */
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '172.46.3.52',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+  return conn;
+};
+
+module.exports = {connect};
